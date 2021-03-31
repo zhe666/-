70ba56cf1de954fe69ce568cbe5df5ae67f86e7f
zhe666 <476106934@qq.com>
2021-03-31 13:24:49 +0800

    设为静态资源目录

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 // 创建express服务器并开始监听
-const express =require("express")
+const express =require("express") //引入模块
 const server=express()
-
+const cors=require('cors') 
+server.use(cors())
+// 设置uploads为静态资源目录
+server.use('/uploads',express.static('uploads'))
 // 开启监听
 server.listen(3000,()=>{
     console.log("服务器已经在3000端口就绪");
